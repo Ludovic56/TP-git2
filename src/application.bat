d1a2f6a675555392c145b0e4319af506ed275eb3
Ludovic Le Cornec <l.le.cornec@iia-laval.fr>
2016-10-11 16:14:15 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
-### application.bat - 2016 - LE CORNEC ###
-echo "Copyright company - 2016"
-echo "Build 10:00"
-echo "version 1.1"
+### application.bat - 201x - LE CORNEC ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
